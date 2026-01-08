f1f21de09b81c2b0ee3862f1ed4f0d92800e97ab
bigbadoooff <engler.mar@googlemail.com>
2026-01-08 01:00:47 +0100

    fixed decode_type esp_mant

--- sensor.py
+++ sensor.py
@@ -28,6 +28,8 @@ from homeassistant.helpers.typing import StateType
 from .register_maps.register_map_manager import RegisterMapManager
 from .sensor_meta import SENSOR_META
 from .thz_device import THZDevice
+import math
+import struct
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -114,11 +116,13 @@ def decode_value(raw: bytes, decode_type: str, factor: float = 1.0):
         # _LOGGER.debug(f"Decode bit {bitnum} from raw {raw.hex()}")
         return not ((raw[0] >> bitnum) & 0x01)
     if decode_type == "esp_mant":
-        # Dummy Beispiel für spezielle Darstellung
-        mant = int.from_bytes(raw[:4], byteorder="big")
-        exp = int.from_bytes(raw[4:], byteorder="big")
-        return mant * (2**exp)
-
+        mant = int.from_bytes(raw[:2], byteorder="big")
+        esp = int.from_bytes(raw[2:], byteorder="big")
+        # The value is encoded as mantissa, exponent (e.g., "123,4" means mant=123, esp=4)
+        # To mimic: sprintf("%.3f", unpack('f', pack('L', reverse(hex($value)))))
+        mant = struct.unpack('<f', raw)[0]
+        return round(mant, 3)
+    
     return raw.hex()
 
 
